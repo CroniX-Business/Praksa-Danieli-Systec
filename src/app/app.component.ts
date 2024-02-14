5c90b8835052d044560ece6da9c46c6432354102
DBraja1738 <dragan.brajkovic091@hotmail.com>
2024-02-14 10:27:16 +0100

    complete calculator app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CalculatorComponent } from './components/calculator/calculator.component';
+import { DraganComponent } from './components/dragan/dragan.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CalculatorComponent],
+  imports: [RouterOutlet,DraganComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
