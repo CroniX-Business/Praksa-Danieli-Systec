70a1569484181504455f1b85bbfbd54900e636dc
blajbaca <blajbaca@gmail.com>
2024-02-14 10:14:08 +0100

    Inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BrunoCalculatorComponent } from '../components/bruno-calculator/bruno-calculator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,BrunoCalculatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/components/bruno-calculator/bruno-calculator.component.ts
@@ -0,0 +1,61 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-bruno-calculator',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './bruno-calculator.component.html',
+  styleUrl: './calculator.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BrunoCalculatorComponent implements OnInit {
+
+  input: any;
+
+  ngOnInit(): void {
+    this.input = new FormGroup({
+      text: new FormControl()
+    }
+    )
+  }
+
+  appendCharacter(buttonElem: any) {
+    let inputValue = buttonElem.textContent
+    if(this.input.controls.text.value!=null){
+      this.input.controls.text.setValue(this.input.controls.text.value+inputValue);
+    }else{
+      this.input.controls.text.setValue(inputValue)
+    }
+  }
+
+  evaluateExpression(){
+    let result;
+    try {
+      result=eval(this.input.controls.text.value)
+      this.input.controls.text.setValue(result.toString())
+    } catch (error) {
+      this.input.controls.text.setValue("Invalid")
+    }
+  }
+
+  removeLastCharacter(){
+    let text=this.input.controls.text.value
+    if(text===null){
+      return
+    }
+    else if(text==="Invalid"){
+      this.input.controls.text.setValue(null)
+    }
+    else{
+      this.input.controls.text.setValue(text.substring(0,text.length-1))
+    }
+  }
+
+  clearDisplay(){
+    this.input.controls.text.setValue(null);
+  }
+}
