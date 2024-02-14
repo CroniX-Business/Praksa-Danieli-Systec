c10c606c52408fd8caf03793c3748dfe57c3be72
Dominik Požarko <dominik.pozarko032@gmail.com>
2024-02-14 13:15:42 +0100

    Deleted js script

--- src/app/Components/dominik-component/Calculator.component.js
+++
@@ -1,9 +0,0 @@
-function inputFunction(val){
-    document.getElementById("Input").innerHTML = "val";
-}
-
-// numbers[9]
-// for (i; i=10; i++){
-//     numbers[i]=document.getElementById(string(i))
-//     document.getElementById(string(i)).onclick="inputFunction(" + i + ")"
-// }
