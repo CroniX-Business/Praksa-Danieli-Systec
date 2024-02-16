147997f57765c00f887de6012d63baaa0afad602
Pruskan <petarruskan@gmail.com>
2024-02-16 14:02:15 +0100

    Add form

--- src/app/pages/petar_login/petar_login.component.ts
+++ src/app/pages/petar_login/petar_login.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { AppConfig } from '../../config/app.config';
 import { AppLanguages } from '../../config/app.languages';
 
@@ -8,6 +9,7 @@ import { AppLanguages } from '../../config/app.languages';
   standalone: true,
   imports: [
     CommonModule,
+    ReactiveFormsModule
   ],
   templateUrl: './petar_login.component.html',
   styleUrl: './petar_login.component.css',
@@ -21,4 +23,18 @@ export class PetarLoginComponent {
   togglePasswordVisibility() {
     this.show = !this.show;    
   }
+
+  applyForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  submitApplication() {
+    let username = this.applyForm.value.username ?? '';
+    let pass = this.applyForm.value.password ?? '';
+
+    console.log(username);
+    console.log(pass);
+  }
+  
 }
