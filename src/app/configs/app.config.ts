6f3eaad34beb01782e4d91f223a65a0bced7c565
blajbaca <blajbaca@gmail.com>
2024-02-16 10:35:25 +0100

    HTML and CSS structured more precisely, added test form method, added config files

---
+++ src/app/configs/app.config.ts
@@ -0,0 +1,7 @@
+export const config =
+{
+    title: 'Angular Demo App',
+    company: 'Danieli-Systec d.o.o.',
+    copyright: `Copyright &copy; ${new Date().getFullYear()} &mdash; {Danieli-Systec d.o.o.}`,
+    defaultLanguage: 'en',
+}
--- src/app/pages/bruno_login/bruno_login.component.ts
+++ src/app/pages/bruno_login/bruno_login.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { NgFor } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { config } from '../../configs/app.config';
+import { languages } from '../../configs/app-languages.config';
 
 @Component({
   selector: 'app-bruno-login',
@@ -8,28 +11,29 @@ import { NgFor } from '@angular/common';
   imports: [
     CommonModule,
     NgFor,
+    ReactiveFormsModule,
   ],
   templateUrl: './bruno_login.component.html',
   styleUrl: './bruno_login.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BrunoLoginComponent { 
-  languages=[
-    {
-      code:"en",
-      name:"English"
-    },
-    {
-      code:"de",
-      name:"German"
-    },
-    {
-      code:"fr",
-      name:"French"
-    },
-    {
-      code:"hr",
-      name:"Croatian"
-    }
-  ]
+export class BrunoLoginComponent implements OnInit {
+
+  time = new Date()
+  form: any
+  config = config
+  languages = languages
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl()
+    })
+  }
+
+  log(){
+    console.log(this.form.controls.username.value.toString())
+    console.log(this.form.controls.password.value.toString())
+  }
+
 }
