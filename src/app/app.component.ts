2693c4ca22c980046254710f959d5c1f03a3517f
DBraja1738 <dragan.brajkovic091@hotmail.com>
2024-02-19 13:47:37 +0100

    make new login with bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 import { DominikComponentComponent } from "../components/dominik-component/dominik-component.component";
 import { PetarComponentComponent } from "../components/petar-component/petar-component.component";
 import { DarioComponentComponent } from "../components/dario-component/dario-component.component";
@@ -13,7 +15,7 @@ import { DraganLoginComponent } from '../components/dragan_login/dragan_login.co
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, PetarComponentComponent, DominikComponentComponent, DarioComponentComponent,DraganComponent,BrunoCalculatorComponent, DraganLoginComponent]
+    imports: [RouterOutlet, PetarComponentComponent, FormsModule, DominikComponentComponent, DarioComponentComponent,DraganComponent,BrunoCalculatorComponent, DraganLoginComponent,]
 })
 export class AppComponent {
 }
