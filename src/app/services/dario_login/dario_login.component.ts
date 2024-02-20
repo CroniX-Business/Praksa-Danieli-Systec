378ff4d4fd668963ddad9ac2b3f54b8de7b466df
dariovidovic <malivida00@gmail.com>
2024-02-20 11:13:19 +0100

    add form validation

--- src/app/services/dario_login/dario_login.component.ts
+++ src/app/services/dario_login/dario_login.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AppConfig } from '../../../app/configs/app.config';
 import { AppLanguagesConfig } from '../../../app/configs/app-languages.config';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-dario-login',
   standalone: true,
   imports: [
-    CommonModule,
+    CommonModule, ReactiveFormsModule 
   ],
   templateUrl: './dario_login.component.html',
   styleUrl: './dario_login.component.css',
@@ -20,8 +21,20 @@ export class DarioLoginComponent {
   appCFG = AppConfig;
   appLCFG = AppLanguagesConfig;
 
+
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
+  }
+
+  loginGroup = new FormGroup(
+    {
+      password: new FormControl('', Validators.required),
+      username: new FormControl('', Validators.required)
+    }
+  );
+
+  test(){
+    console.log(this.loginGroup.value.password, this.loginGroup.value.username);
   }
 
  }
