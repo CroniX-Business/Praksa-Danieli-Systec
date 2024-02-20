cafcd330eb0850650e9eaa52126e7ccde93c195b
Dominik Požarko <dominik.pozarko032@gmail.com>
2024-02-20 15:09:08 +0100

    Clearing code (1)

--- src/app/pages/Dominik_login/Dominik_login.component.ts
+++ src/app/pages/Dominik_login/Dominik_login.component.ts
@@ -14,33 +14,16 @@ import { FormsModule, FormControl, FormGroup, ReactiveFormsModule } from '@angul
   styleUrl: './Dominik_login.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
+
 export class DominikLoginComponent { 
   Lan=Languages;
   showPassword=false
   appCnfg=appConfiguration;
-  passwordstate = "Password"
-  Usernamewarning=""
-  Passwordwarning=""
-  Usernameselected=false
-  Passwordselected=false
-  btndisabled="disabled"
-  
-  
-  
+
   applyform = new FormGroup({
     userName: new FormControl(''),
     userPassword: new FormControl('')
   });
 
   submitFunction(){}
-
-
-  change_pass_state(){
-    if (this.passwordstate=="Password"){
-      this.passwordstate="Text"
-      this.showPassword=true}
-    else{
-      this.passwordstate="Password"
-      this.showPassword=false}
-  }
 }
