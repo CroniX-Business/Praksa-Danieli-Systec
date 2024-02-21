1645d7b2510c65ea88c4296c6099dd21d5f21a83
blajbaca <blajbaca@gmail.com>
2024-02-21 11:29:50 +0100

    Lint fixes

--- src/app/pages/bruno_login/bruno_login.component.ts
+++ src/app/pages/bruno_login/bruno_login.component.ts
@@ -1,41 +1,41 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgFor } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { config } from '../../configs/app.config';
 import { languages } from '../../configs/app-languages.config';
 
 @Component({
   selector: 'app-bruno-login',
   standalone: true,
-  imports: [
-    CommonModule,
-    NgFor,
-    ReactiveFormsModule,
-  ],
+  imports: [CommonModule, NgFor, ReactiveFormsModule],
   templateUrl: './bruno_login.component.html',
   styleUrl: './bruno_login.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BrunoLoginComponent{
-
-  time = new Date()
-  form = new FormGroup({
+export class BrunoLoginComponent {
+  public time = new Date();
+  public form = new FormGroup({
     username: new FormControl(null, Validators.required),
-    password: new FormControl(null, [Validators.minLength(7),Validators.required])
-  })
-  config = config
-  languages = languages
-  fieldTextType: boolean=false;
+    password: new FormControl(null, [
+      Validators.minLength(7),
+      Validators.required,
+    ]),
+  });
+  public config = config;
+  public languages = languages;
+  public fieldTextType: boolean = false;
 
-  
-
-  toggleFieldTextType(){
-    this.fieldTextType= !this.fieldTextType
+  public toggleFieldTextType(): void {
+    this.fieldTextType = !this.fieldTextType;
   }
 
-  log(){
-    console.log(this.form.value)
+  public log(): void {
+    console.log(this.form.value);
   }
-
 }
