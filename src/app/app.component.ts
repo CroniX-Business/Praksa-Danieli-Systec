fffad4e07d23a5704392c9452e8694cd8f4cdc8d
Dominik Požarko <dominik.pozarko032@gmail.com>
2024-02-21 11:14:58 +0100

    Fixed errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DominikLoginComponent } from './pages/Dominik_login/Dominik_login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DominikLoginComponent],
 })
-     
 export class AppComponent {
   public title: string = 'my-angular-project';
 }
--- src/app/pages/Dominik_login/Dominik_login.component.ts
+++ src/app/pages/Dominik_login/Dominik_login.component.ts
@@ -2,28 +2,29 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Languages } from '../../configs/app-languages.config';
 import { appConfiguration } from '../../configs/app.config';
-import { match } from 'node:assert';
-import { FormsModule, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormsModule,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 @Component({
   selector: 'app-dominik-login',
   standalone: true,
-  imports: [
-    CommonModule,ReactiveFormsModule, FormsModule
-  ],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './Dominik_login.component.html',
   styleUrl: './Dominik_login.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
+export class DominikLoginComponent {
+  public Lan = Languages;
+  public showPassword = false;
+  public appCnfg = appConfiguration;
 
-export class DominikLoginComponent { 
-  Lan=Languages;
-  showPassword=false
-  appCnfg=appConfiguration;
-
-  applyform = new FormGroup({
+  public applyform = new FormGroup({
     userName: new FormControl(''),
-    userPassword: new FormControl('')
+    userPassword: new FormControl(''),
   });
 
-  submitFunction(){}
+  public submitFunction(): void {}
 }
