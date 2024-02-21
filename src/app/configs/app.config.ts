9d237e3f811ce415141ecbebabfd85c0c10d4596
dariovidovic <malivida00@gmail.com>
2024-02-21 10:18:30 +0100

    fix warnings and errors

--- src/app/configs/app.config.ts
+++ src/app/configs/app.config.ts
@@ -1,6 +1,6 @@
-export const AppConfig: any = {
-    title: 'Angular Demo App',
-    company: 'Danieli-Systec d.o.o.',
-    copyright: `Copyright &copy; ${new Date().getFullYear()} &mdash; Danieli-Systec d.o.o.`,
-    defaultLanguage: 'en',
-}
+export const AppConfig = {
+  title: 'Angular Demo App',
+  company: 'Danieli-Systec d.o.o.',
+  copyright: `Copyright &copy; ${new Date().getFullYear()} &mdash; Danieli-Systec d.o.o.`,
+  defaultLanguage: 'en',
+};
--- src/app/pages/dario_login/dario_login.component.ts
+++ src/app/pages/dario_login/dario_login.component.ts
@@ -2,31 +2,29 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AppConfig } from '../../configs/app.config';
 import { AppLanguagesConfig } from '../../configs/app-languages.config';
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dario-login',
   standalone: true,
-  imports: [
-    CommonModule, ReactiveFormsModule 
-  ],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './dario_login.component.html',
   styleUrl: './dario_login.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DarioLoginComponent {
+  public showPassword: boolean = false;
 
-  showPassword: Boolean = false;
+  public appCFG = AppConfig;
+  public appLCFG = AppLanguagesConfig;
 
-  appCFG = AppConfig;
-  appLCFG = AppLanguagesConfig;
-
-
-  loginGroup = new FormGroup(
-    {
-      password: new FormControl('', Validators.required),
-      username: new FormControl('', Validators.required)
-    }
-  );
-
- }
+  public loginGroup = new FormGroup({
+    password: new FormControl('', Validators.required),
+    username: new FormControl('', Validators.required),
+  });
+}
