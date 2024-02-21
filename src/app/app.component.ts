33b7a843676b346c786a1aad9e623c6da1d0b264
Davor Božanić <d.bozanic@systec.danieli.com>
2024-02-21 10:07:23 +0100

    Cleaned code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DominikComponentComponent } from "../components/dominik-component/dominik-component.component";
-import { PetarComponentComponent } from "../components/petar-component/petar-component.component";
-import { DarioComponentComponent } from "../components/dario-component/dario-component.component";
-import { DraganComponent } from '../components/dragan/dragan.component';
-import { BrunoCalculatorComponent } from '../components/bruno-calculator/bruno-calculator.component';
-
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, PetarComponentComponent, DominikComponentComponent, DarioComponentComponent,DraganComponent,BrunoCalculatorComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet],
 })
 export class AppComponent {
-  public title: string = "my-angular-project";
+  public title: string = 'my-angular-project';
 }
