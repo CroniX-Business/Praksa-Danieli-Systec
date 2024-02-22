d946a2ef8f5c054dc50163ba33d635885ea45d37
Dominik Požarko <dominik.pozarko032@gmail.com>
2024-02-22 12:34:01 +0100

    Fixed merge errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { DominikLoginComponent } from './pages/Dominik_login/Dominik_login.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, DominikLoginComponent],
 })
 export class AppComponent {
   public title: string = 'my-angular-project';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DominikLoginComponent } from './pages/Dominik_login/Dominik_login.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, DominikLoginComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
