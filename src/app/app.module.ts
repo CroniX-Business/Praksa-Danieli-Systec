3fca18a01128c671c805b771988404c00f97ee8b
dariovidovic <malivida00@gmail.com>
2024-02-22 11:18:37 +0100

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DarioLoginComponent } from './pages/dario_login/dario_login.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, DarioLoginComponent],
 })
 export class AppModule {}
