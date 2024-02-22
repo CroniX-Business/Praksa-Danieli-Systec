ab3e6e94ab168f081d8bc935d4d45b6d093f7a80
blajbaca <blajbaca@gmail.com>
2024-02-22 15:07:21 +0100

    Inital register page commit, email and password confirm error messages tbd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrunoLoginComponent } from './pages/bruno_login/bruno_login.component';
+import { BrunoRegisterComponentComponent } from './pages/bruno_register/bruno_register_component/bruno_register.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrunoLoginComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrunoLoginComponent,
+    BrunoRegisterComponentComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/pages/bruno_register/bruno_register_component/bruno_register.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { config } from '../../../configs/app.config';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-bruno-register',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './bruno_register.component.html',
+  styleUrl: './bruno_register.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BrunoRegisterComponentComponent {
+  public usernameRegex: RegExp = /^(?=.{4,20}$)[a-zA-Z0-9_.]+$/;
+  public passwordRegex: RegExp =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{8,}$/;
+  public config = config;
+  public fieldTextType: boolean = false;
+  public form = new FormGroup({
+    username: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(this.usernameRegex),
+    ]),
+    password: new FormControl(null, [
+      Validators.minLength(8),
+      Validators.pattern(this.passwordRegex),
+      Validators.required,
+    ]),
+    confirmPassword: new FormControl(null, [
+      Validators.minLength(8),
+      Validators.required,
+    ]),
+    firstName: new FormControl(null, Validators.required),
+    lastName: new FormControl(null, Validators.required),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+  });
+}
