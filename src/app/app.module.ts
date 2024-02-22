ee0892d5a1fd5b9a98e50f68f661a5ef298ff2a7
dariovidovic <malivida00@gmail.com>
2024-02-22 15:03:08 +0100

    finished register component design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DarioLoginComponent } from './pages/dario_login/dario_login.component';
+import { DarioRegisterComponent } from './pages/dario_register/dario_register.component';
 
 @NgModule({
   declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, DarioLoginComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DarioLoginComponent,
+    DarioRegisterComponent,
+  ],
 })
 export class AppModule {}
--- src/app/pages/dario_register/dario_register.component.ts
+++ src/app/pages/dario_register/dario_register.component.ts
@@ -3,8 +3,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AppConfig } from '../../configs/app.config';
 import { AppLanguagesConfig } from '../../configs/app-languages.config';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
-
 import {
   FormControl,
   FormGroup,
@@ -13,39 +11,41 @@ import {
 } from '@angular/forms';
 
 @Component({
-  selector: 'app-dario-login',
+  selector: 'app-dario-register',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
-  templateUrl: './dario_login.component.html',
-  styleUrl: './dario_login.component.css',
+  templateUrl: './dario_register.component.html',
+  styleUrl: './dario_register.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DarioLoginComponent {
+export class DarioRegisterComponent {
   public showPassword: boolean = false;
+  public showConfirmPassword: boolean = false;
 
   public appCFG = AppConfig;
   public appLCFG = AppLanguagesConfig;
   public loginMessage: string | null = null;
 
-  public constructor(
-    private authService: AuthService,
-    private router: Router
-  ) {}
+  public constructor(private authService: AuthService) {}
 
-  public loginGroup = new FormGroup({
-    password: new FormControl('', Validators.required),
+  public registerGroup = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
     username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    confirmPassword: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
   });
 
   public onSubmit(): void {
     if (
-      this.loginGroup.controls.username.value != null &&
-      this.loginGroup.controls.password.value != null
+      this.registerGroup.controls.username.value != null &&
+      this.registerGroup.controls.password.value != null
     ) {
       this.authService
         .logIn(
-          this.loginGroup.controls.username.value,
-          this.loginGroup.controls.password.value
+          this.registerGroup.controls.username.value,
+          this.registerGroup.controls.password.value
         )
         .subscribe(value => {
           if (value) {
@@ -55,8 +55,8 @@ export class DarioLoginComponent {
 
       console.log(
         this.authService.logIn(
-          this.loginGroup.controls.username.value,
-          this.loginGroup.controls.password.value
+          this.registerGroup.controls.username.value,
+          this.registerGroup.controls.password.value
         )
       );
     }
