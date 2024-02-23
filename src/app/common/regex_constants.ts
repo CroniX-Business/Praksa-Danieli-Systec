3adcbec0e949737301dc7cd625e4835dedfeb377
DBraja1738 <dragan.brajkovic091@hotmail.com>
2024-02-23 15:06:59 +0100

    add regex, checks for equal password

---
+++ src/app/common/regex_constants.ts
@@ -0,0 +1,2 @@
+export const EmailRegex: RegExp = /^[a-zA-Z0-9]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+export const NameRegex: RegExp = /^[a-zA-Z]+$/;
--- src/app/pages/dragan_register/dragan_register.component.ts
+++ src/app/pages/dragan_register/dragan_register.component.ts
@@ -3,6 +3,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { appConfig } from '../../configs/app.config';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NameRegex } from '../../common/regex_constants';
+import { EmailRegex } from '../../common/regex_constants';
 @Component({
   selector: 'app-dragan-register',
   standalone: true,
@@ -17,10 +19,26 @@ export class DraganRegisterComponent {
 
   public registerMessage: string | null = null;
   public myForm = new FormGroup({
-    firstName: new FormControl('', Validators.required),
-    lastName: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
+    firstName: new FormControl('', [
+      Validators.required,
+      Validators.pattern(NameRegex),
+    ]),
+    lastName: new FormControl('', [
+      Validators.required,
+      Validators.pattern(NameRegex),
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern(EmailRegex),
+    ]),
     username: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    repeatPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
   });
 }
