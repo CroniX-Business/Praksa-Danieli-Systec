0b20c6a4100e3ae426fe3ac91461f1d7ae7b66f9
Pruskan <petarruskan@gmail.com>
2024-02-26 12:19:50 +0100

    Small fix

--- src/app/common/regex_constants.ts
+++ src/app/common/regex_constants.ts
@@ -3,3 +3,5 @@ export const passwordRegex: RegExp =
 export const usernameRegex: RegExp = /^[a-zA-Z0-9_.]+$/;
 export const emailRegex: RegExp = /^([\w.-]+)@([\w-]+)((\.(\w){2,9})+)$/;
 export const nameRegex: RegExp = /^(?=.{1,40}$)[a-zA-Z]+(?:[-'\s][a-zA-Z]+)*$/;
+
+// ^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$
