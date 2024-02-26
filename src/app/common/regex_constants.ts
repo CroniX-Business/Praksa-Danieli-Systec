662cde817e37d3c39d07c3a9f39138377a74c77c
DBraja1738 <dragan.brajkovic091@hotmail.com>
2024-02-26 11:08:41 +0100

    fix email regex

--- src/app/common/regex_constants.ts
+++ src/app/common/regex_constants.ts
@@ -1,3 +1,3 @@
-export const EmailRegex: RegExp = /^[a-zA-Z0-9]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+export const EmailRegex: RegExp = /^([\w.-]+)@([\w-]+)((\.(\w){2,9})+)$/;
 export const NameRegex: RegExp = /^[a-zA-Z]+$/;
 export const UsernameRegex: RegExp = /^[a-zA-Z0-9_.]{3,20}$/;
