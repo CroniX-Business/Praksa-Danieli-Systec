6cb8442f69e24f3363dad4a565728b52f13d72dd
DBraja1738 <dragan.brajkovic091@hotmail.com>
2024-02-27 13:19:55 +0100

    change routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { DraganLoginComponent } from './pages/dragan_login/dragan_login.component';
 import { DraganRegisterComponent } from './pages/dragan_register/dragan_register.component';
 import { PageNotFoundComponent } from './pages/pageNotFound/pageNotFound.component';
-
+import { routesConfig } from './configs/routes.config';
+import { routeNames } from './configs/routes.config';
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: DraganLoginComponent },
-  { path: 'register', component: DraganRegisterComponent },
+  { path: '', redirectTo: routesConfig.login, pathMatch: 'full' },
+  { path: routeNames.login, component: DraganLoginComponent },
+  { path: routeNames.register, component: DraganRegisterComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
--- src/app/configs/routes.config.ts
+++ src/app/configs/routes.config.ts
@@ -3,3 +3,8 @@ export const routesConfig = {
   register: '/register',
   error404: '/error404',
 };
+export const routeNames = {
+  login: 'login',
+  register: 'register',
+  error404: 'error404',
+};
