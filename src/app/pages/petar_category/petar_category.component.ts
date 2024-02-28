c8bccfa4be50d3dd358349cb87b1512e7edfcb7e
Pruskan <petarruskan@gmail.com>
2024-02-28 13:41:56 +0100

    Fix lint

--- src/app/pages/petar_category/petar_category.component.ts
+++ src/app/pages/petar_category/petar_category.component.ts
@@ -2,15 +2,21 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AppRoutesConfig } from '../../config/routes.config';
-import { PetarHomepageComponent } from "../petar_homepage/petar_homepage.component";
+import { PetarHomepageComponent } from '../petar_homepage/petar_homepage.component';
 
 @Component({
-    selector: 'app-petar-category',
-    standalone: true,
-    templateUrl: './petar_category.component.html',
-    styleUrl: './petar_category.component.css',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet, PetarHomepageComponent]
+  selector: 'app-petar-category',
+  standalone: true,
+  templateUrl: './petar_category.component.html',
+  styleUrl: './petar_category.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+    PetarHomepageComponent,
+  ],
 })
 export class PetarCategoryComponent {
   public AppRoutesConfig = AppRoutesConfig;
--- src/app/pages/petar_restaurant/petar_restaurant.component.ts
+++ src/app/pages/petar_restaurant/petar_restaurant.component.ts
@@ -2,15 +2,21 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AppRoutesConfig } from '../../config/routes.config';
-import { PetarHomepageComponent } from "../petar_homepage/petar_homepage.component";
+import { PetarHomepageComponent } from '../petar_homepage/petar_homepage.component';
 
 @Component({
-    selector: 'app-petar-restaurant',
-    standalone: true,
-    templateUrl: './petar_restaurant.component.html',
-    styleUrl: './petar_restaurant.component.css',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet, PetarHomepageComponent]
+  selector: 'app-petar-restaurant',
+  standalone: true,
+  templateUrl: './petar_restaurant.component.html',
+  styleUrl: './petar_restaurant.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+    PetarHomepageComponent,
+  ],
 })
 export class PetarRestaurantComponent {
   public AppRoutesConfig = AppRoutesConfig;
