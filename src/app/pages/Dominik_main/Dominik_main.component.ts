04acba1acb9ba5b3f9e0f05ea1247d4ca7ff74c4
Dominik Požarko <dominik.pozarko032@gmail.com>
2024-02-28 10:03:04 +0100

    Main page edited

--- src/app/pages/Dominik_main/Dominik_main.component.ts
+++ src/app/pages/Dominik_main/Dominik_main.component.ts
@@ -12,6 +12,6 @@ import { RouterLinkActive, RouterOutlet, RouterLink } from '@angular/router';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DominikMainComponent {
-  public restocollapsed = true;
   public routesConfig = AppRoutesConfig.routeConfig;
+  public sidebar = false;
 }
