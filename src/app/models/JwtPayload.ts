44b92de4f7b71a0ed8dd83482c28ac43760f2264
Pruskan <petarruskan@gmail.com>
2024-03-01 10:11:05 +0100

    Fix JwtPayload class

--- src/app/models/JwtPayload.ts
+++ src/app/models/JwtPayload.ts
@@ -1,8 +1,5 @@
-import moment from 'moment';
-
 export class JwtPayload {
   public expires_at: number;
-  public expireTime: number = moment().unix();
 
   public constructor(expires_at: number) {
     this.expires_at = expires_at;
