21b28da17d14db4efeb2dcf30513e958db447075
Pruskan <petarruskan@gmail.com>
2024-03-04 11:13:15 +0100

    Add navigate to login in canActivate if false

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,17 +2,22 @@ import { Injectable } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { AppRoutesConfig } from '../config/routes.config';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthGuard {
   public AppRoutesConfig = AppRoutesConfig;
 
-  public constructor(private authService: AuthService) {}
+  public constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   public canActivate: CanActivateFn = () => {
     if (this.authService.isLoggedIn()) {
       return true;
     }
+    this.router.navigate([AppRoutesConfig.routeNames.login]);
     return false;
   };
 }
